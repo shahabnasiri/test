b399178ecc5f4316865321d00146b4c3f18dfeba
Shahab <125216818+shahabnasiri@users.noreply.github.com>
2024-11-24 01:13:18 +0330

    Update

---
+++ quick-sort.py
@@ -0,0 +1,13 @@
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]  # انتخاب محور
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quick_sort(left) + middle + quick_sort(right)
+
+# نمونه استفاده
+arr = [10, 7, 8, 9, 1, 5]
+sorted_arr = quick_sort(arr)
+print(sorted_arr)
